7fc471a17d78490015b16a1612f496dac583049b
Gagn-kainth <preetgagan710@gmail.com>
2026-06-05 23:26:00 +0530

    added directory structure made models,routes->get ,post ,added async await,try catch

--- controllers/url.js
+++ controllers/url.js
@@ -1,22 +1,26 @@
-const Person = require('../models/person');
+const Person = require("../models/person");
 
 async function HandleHotelHome(req, res) {
-    res.send('Welcome to the Hotel M. How may I assist you?');
+  res.send("Welcome to the Hotel M. How may I assist you?");
 }
 
 async function HandleNewPerson(req, res) {
-    try {
-        const data = req.body;
-        const newPerson = new Person(data);
-        await newPerson.save();
-        res.status(201).json({ message: 'Person added successfully', person: newPerson });
-    } catch (error) {
-        console.error('Error adding person:', error);
-        res.status(500).json({ error: 'An error occurred while adding the person.' });
-    }
+  try {
+    const data = req.body;
+    const newPerson = new Person(data);
+    await newPerson.save();
+    res
+      .status(201)
+      .json({ message: "Person added successfully", person: newPerson });
+  } catch (error) {
+    console.error("Error adding person:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while adding the person." });
+  }
 }
 
 module.exports = {
-    HandleHotelHome,
-    HandleNewPerson
+  HandleHotelHome,
+  HandleNewPerson,
 };
